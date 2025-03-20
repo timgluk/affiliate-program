3f95fadd3a3cc37582b11d6a9539e2a06425c816
timgluk <timi.biz@bk.ru>
2025-03-20 22:49:03 +0300

    add docker

---
+++ api/service/version.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode([
+    'version' => 1.0,
+]);
+die;
---
+++ views/pages/index.php
@@ -0,0 +1,5 @@
+<?php include 'views/components/header.php'; ?>
+
+<h1>Hello world</h1>
+
+<?php include 'views/components/footer.php'; ?>
